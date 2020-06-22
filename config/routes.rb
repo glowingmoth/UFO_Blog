30ed5f35aabaaab2abd06ac8396d4d6adb3883e5
Thomas Doig <thomasdoig@hotmail.com>
2020-06-22 15:53:52 +1000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'blogs/index' 
+  get 'blogs/new'
+  get 'blogs/show'
+  get 'blogs/create'
+  get 'blogs/upate'
+  get 'blogs/edit'
+  get 'blogs/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
