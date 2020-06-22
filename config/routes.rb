87678146c0429e81709c123fbc37620b17649489
Thomas Doig <thomasdoig@hotmail.com>
2020-06-22 16:19:31 +1000

    created routes for blog and linked heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'blogs/index' 
-  get 'blogs/new'
-  get 'blogs/show'
-  get 'blogs/create'
-  get 'blogs/upate'
-  get 'blogs/edit'
-  get 'blogs/destroy'
+  get 'blogs', to: "blogs#index", as: "blogs"
+  get 'blogs/new', to: "blogs#new", as: "blog_new" 
+  get 'blogs/:id', to: "blogs#show", as: "blog"
+  post 'blogs', to: "blogs#create"
+  put 'blogs/:id', to: "blog#update"
+  get 'blogs/:id/edit', to: "blog#edit", as: "blog_edit"
+  get 'blogs/:id', to: "blog#destroy"
+
+  root "blogs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
